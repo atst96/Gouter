ce18037d07a085063997d98d19bcedab78e4c7b6
atst1996 <tnkjpei04@gmail.com>
2019-03-04 14:43:37 +0900

    設定関連クラスの作成

---
+++ Gouter/Settings/ApplicationSetting.cs
@@ -0,0 +1,22 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gouter
+{
+    [MessagePackObject]
+    internal class ApplicationSetting : NotificationObject
+    {
+        [Key("directory.musics")]
+        private List<string> _musicDirectories;
+
+        [IgnoreMember]
+        public List<string> MusicDirectories
+        {
+            get => this._musicDirectories ?? (this._musicDirectories = new List<string>());
+        }
+    }
+}
