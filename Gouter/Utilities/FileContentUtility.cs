a87db04d24c9462a6ab781e1a057c81bb5b65aff
atst1996 <tnkjpei04@gmail.com>
2019-03-04 15:18:57 +0900

    ファイル読み書き関連のユーティリティクラスの実装

---
+++ Gouter/Utilities/FileContentUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gouter.Utilities
+{
+    internal static class FileContentUtility
+    {
+        public static FileStream OpenRead(string path)
+        {
+            var fileInfo = new FileInfo(path);
+
+            return fileInfo.Exists && fileInfo.Length > 0
+                ? fileInfo.OpenRead()
+                : null;
+        }
+
+        public static FileStream OpenCreate(string path)
+        {
+            return File.Open(path, FileMode.Create, FileAccess.Write);
+        }
+    }
+}
---
+++ Gouter/Utilities/MessagePackUtility.cs
@@ -0,0 +1,52 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gouter.Utilities
+{
+    internal static class MessagePackUtility
+    {
+        private static readonly IFormatterResolver _messagPackResolver = MessagePack.Resolvers.StandardResolverAllowPrivate.Instance;
+
+        public static async ValueTask<T> DeserializeFile<T>(string path)
+        {
+            var stream = FileContentUtility.OpenRead(path);
+            if (stream == null)
+            {
+                return default;
+            }
+
+            using (stream)
+            {
+                return await DeserializeAsync<T>(stream).ConfigureAwait(false);
+            }
+        }
+
+        public static Task<T> DeserializeAsync<T>(Stream stream)
+        {
+            return MessagePackSerializer.DeserializeAsync<T>(stream, _messagPackResolver);
+        }
+
+        public static async Task SerializeFile<T>(T @object, string path)
+        {
+            using (var bufferStream = new MemoryStream())
+            {
+                await SerializeAsync(@object, bufferStream).ConfigureAwait(false);
+
+                using (var outputStream = FileContentUtility.OpenCreate(path))
+                {
+                    await bufferStream.CopyToAsync(outputStream).ConfigureAwait(false);
+                }
+            }
+        }
+
+        public static Task SerializeAsync<T>(T @object, Stream stream)
+        {
+            return MessagePackSerializer.SerializeAsync(stream, @object, _messagPackResolver);
+        }
+    }
+}
