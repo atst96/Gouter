4a6b73cca4e4304689a1ff5c146a624486a33d91
atst1996 <tnkjpei04@gmail.com>
2019-03-06 00:27:43 +0900

    GeometryIconの実装

---
+++ Gouter/Controls/GeometryIcon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Gouter
+{
+    internal class GeometryIcon : Control
+    {
+        public Brush Fill
+        {
+            get => (Brush)this.GetValue(FillProperty);
+            set => this.SetValue(FillProperty, value);
+        }
+
+        public static readonly DependencyProperty FillProperty =
+            DependencyProperty.Register("Fill", typeof(Brush), typeof(GeometryIcon), new PropertyMetadata(null));
+
+        public Geometry Data
+        {
+            get => (Geometry)this.GetValue(DataProperty);
+            set => this.SetValue(DataProperty, value);
+        }
+
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register("Data", typeof(Geometry), typeof(GeometryIcon), new PropertyMetadata(null));
+    }
+}
