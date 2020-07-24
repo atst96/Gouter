b6be1a9679b5190d1da64ed430a4029da4a2b767
atst1996 <tnkjpei04@gmail.com>
2020-07-24 12:40:43 +0900

    improve: アルバム画像保持処理の修正

--- Gouter/MediaPlayer/AlbumInfo.cs
+++ Gouter/MediaPlayer/AlbumInfo.cs
@@ -1,12 +1,8 @@
 ﻿using ATL;
 using Gouter.DataModels;
-using Gouter.Utilities;
+using Gouter.Utils;
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Windows;
 using System.Windows.Media;
 
 namespace Gouter
@@ -16,32 +12,48 @@ namespace Gouter
     /// </summary>
     internal class AlbumInfo : NotificationObject, IPlaylistInfo
     {
-        /// <summary>アルバムの内部ID</summary>
+        /// <summary>
+        /// アルバムの内部ID
+        /// </summary>
         public int Id { get; }
 
-        /// <summary>アルバム識別キー</summary>
+        /// <summary>
+        /// アルバム識別キー
+        /// </summary>
         public string Key { get; }
 
-        /// <summary>アルバム名</summary>
+        /// <summary>
+        /// アルバム名
+        /// </summary>
         public string Name { get; private set; }
 
-        /// <summary>アーティスト名</summary>
+        /// <summary>
+        /// アーティスト名
+        /// </summary>
         public string Artist { get; private set; }
 
-        /// <summary>アルバム情報登録日時</summary>
+        /// <summary>
+        /// アルバム情報登録日時
+        /// </summary>
         public DateTimeOffset RegisteredAt { get; }
 
-        /// <summary>アルバム情報更新日時</summary>
+        /// <summary>
+        /// アルバム情報更新日時
+        /// </summary>
         public DateTimeOffset UpdatedAt { get; private set; }
 
-        /// <summary>アルバムアートの最大サイズ</summary>
+        /// <summary>
+        /// アルバムアートの最大サイズ
+        /// </summary>
         private const int MaxImageSize = 80;
-        
-        /// <summary>トラック情報からアルバム情報を生成する</summary>
+
+        /// <summary>
+        /// トラック情報からアルバム情報を生成する
+        /// </summary>
         /// <param name="id">アルバムID</param>
         /// <param name="key">アルバムキー</param>
         /// <param name="track">トラック情報</param>
-        public AlbumInfo(int id, string key, Track track)
+        public AlbumInfo(int id, string key, Track track, byte[] artwork)
         {
             this.Id = id;
             this.Key = key;
@@ -51,21 +63,17 @@ namespace Gouter
             this.RegisteredAt = DateTimeOffset.Now;
             this.UpdatedAt = this.RegisteredAt;
 
-            var artwork = track.EmbeddedPictures.FirstOrDefault();
-
-            if (artwork?.PictureData?.Length > 0)
+            if (artwork?.Length > 0)
             {
-                this.ArtworkStream = ImageUtility.ShrinkImageData(artwork.PictureData, MaxImageSize);
-            }
-            else
-            {
-                this.Artwork = ImageUtility.GetMissingAlbumImage();
+                this.SetArtworkData(artwork);
             }
 
             this.Playlist = new AlbumPlaylist(this);
         }
 
-        /// <summary>DBのモデルデータからアルバム情報を生成する</summary>
+        /// <summary>
+        /// DBのモデルデータからアルバム情報を生成する
+        /// </summary>
         /// <param name="dataModel">DBモデル</param>
         public AlbumInfo(AlbumDataModel dataModel)
         {
@@ -81,49 +89,64 @@ namespace Gouter
 
             if (artwork?.Length > 0)
             {
-                this.ArtworkStream = new MemoryStream(artwork);
-            }
-            else
-            {
-                this.Artwork = ImageUtility.GetMissingAlbumImage();
+                this.SetArtworkData(artwork);
             }
 
             this.Playlist = new AlbumPlaylist(this);
         }
 
-        private MemoryStream _artworkStream;
-        /// <summary>アートワークの生データ</summary>
-        public MemoryStream ArtworkStream
+        private bool _isArtworkFound = false;
+        private byte[] _tempArtworkData = null;
+
+        private object @_lockObj = new object();
+
+        private void SetArtworkData(byte[] imageData)
         {
-            get => this._artworkStream;
-            set
+            lock (this.@_lockObj)
             {
-                if (this.SetProperty(ref this._artworkStream, value))
-                {
-                    if (value == null || value.Length == 0)
-                    {
-                        this.Artwork = ImageUtility.GetMissingAlbumImage();
-                    }
-                    else
-                    {
-                        this.Artwork = ImageUtility.BitmapImageFromStream(value);
-                    }
-                }
+                this._tempArtworkData = imageData;
+                this._isArtworkFound = true;
             }
         }
 
         private ImageSource _artwork;
-        /// <summary>アートワーク</summary>
+        /// <summary>
+        /// アートワーク
+        /// </summary>
         public ImageSource Artwork
         {
-            get => this._artwork;
-            private set => this.SetProperty(ref this._artwork, value);
+            get
+            {
+                lock (this._lockObj)
+                {
+                    if (this._artwork == null)
+                    {
+                        if (!this._isArtworkFound)
+                        {
+                            this._artwork = ImageUtility.GetMissingAlbumImage();
+                        }
+                        else
+                        {
+                            var imageStream = ImageUtility.ShrinkImageData(this._tempArtworkData, MaxImageSize);
+                            this._artwork = ImageUtility.BitmapImageFromStream(imageStream);
+
+                            this._tempArtworkData = null;
+                        }
+                    }
+
+                    return this._artwork;
+                }
+            }
         }
 
-        /// <summary>コンピレーションアルバムか否かのフラグ</summary>
+        /// <summary>
+        /// コンピレーションアルバムか否かのフラグ
+        /// </summary>
         public bool IsCompilation { get; private set; }
 
-        /// <summary>プレイリスト情報</summary>
+        /// <summary>
+        /// プレイリスト情報
+        /// </summary>
         public AlbumPlaylist Playlist { get; }
     }
 }
