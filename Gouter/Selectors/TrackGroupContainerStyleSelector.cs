266a405f20e895bb7f8fe6a0d37cf0a725df9a50
atst1996 <tnkjpei04@gmail.com>
2020-11-22 00:46:13 +0900

    change: ディスク番号が1値しかない場合にヘッダ(ディスク番号)を表示しないように変更

---
+++ Gouter/Selectors/TrackGroupContainerStyleSelector.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Gouter.Extensions;
+
+namespace Gouter.Selectors
+{
+    internal class TrackGroupContainerStyleSelector : StyleSelector
+    {
+        /// <summary>
+        /// ヘッダ無しのスタイル
+        /// </summary>
+        private readonly Style _noHeaderGroupContainerStyle;
+
+        /// <summary>
+        /// ヘッダありのスタイル
+        /// </summary>
+        private readonly Style _headeredGroupContainerStyle;
+
+        public TrackGroupContainerStyleSelector() : base()
+        {
+            var app = App.Instance;
+            this._headeredGroupContainerStyle = app.TryFindResource("HeaderedGroupItemStyle") as Style;
+            this._noHeaderGroupContainerStyle = app.TryFindResource("NoHeaderGroupItemStyle") as Style;
+        }
+
+        public override Style SelectStyle(object item, DependencyObject container)
+        {
+            if (container is GroupItem groupItem)
+            {
+                var itemsControl = groupItem.FindAncestor<ItemsControl>();
+                if (itemsControl is ListBox)
+                {
+                    if (itemsControl.ItemsSource is CollectionView collectionView
+                        && collectionView.Groups.Count <= 1)
+                    {
+                        return this._noHeaderGroupContainerStyle;
+                    }
+                }
+            }
+
+            return this._headeredGroupContainerStyle;
+        }
+    }
+}
