b3d9401079e8d82bf4b73cb67940aed605e62159
atst1996 <tnkjpei04@gmail.com>
2021-06-13 22:45:27 +0900

    add: コントロール追加

---
+++ Gouter/Controls/CornerButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Gouter.Controls
+{
+    internal class CornerButton : Button
+    {
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius)this.GetValue(CornerRadiusProperty);
+            set => this.SetValue(CornerRadiusProperty, value);
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty
+            = DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(CornerButton),
+                new FrameworkPropertyMetadata(new CornerRadius(),
+                    FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender));
+    }
+}
