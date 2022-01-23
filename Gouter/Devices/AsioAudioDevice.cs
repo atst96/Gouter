ceefc7df18dbdf5b29462f368157a610a73df395
atst_96 <tnkjpei04@gmail.com>
2022-01-23 23:24:43 +0900

    add: DirectSound/ASIO用の制御クラスを追加

---
+++ Gouter/Devices/AsioAudioDevice.cs
@@ -0,0 +1,104 @@
+﻿using NAudio.Wave;
+
+namespace Gouter.Devices;
+
+/// <summary>
+/// ASIO音声出力デバイス
+/// </summary>
+internal class AsioAudioDevice : AudioDevice
+{
+    /// <summary>
+    /// オーディオデバイス
+    /// </summary>
+    private string _driverName;
+    private AsioOut _audioRender;
+
+    /// <summary>
+    /// constructor
+    /// </summary>
+    /// <param name="driverName">ASIOドライバ名</param>
+    public AsioAudioDevice(string driverName)
+    {
+        this._driverName = driverName;
+    }
+
+    /// <summary>
+    /// 再生状態
+    /// </summary>
+    public override PlaybackState PlaybackState => this._audioRender?.PlaybackState ?? PlaybackState.Stopped;
+
+    /// <summary>
+    /// オーディオソースを設定する
+    /// </summary>
+    /// <param name="waveProvider"></param>
+    public override void SetAudioSource(ISampleProvider waveProvider) => Invoke(() =>
+    {
+        var render = this.UpdateAudioRender();
+
+        render.Init(waveProvider);
+    });
+
+    /// <summary>
+    /// オーディオレンダラを更新する
+    /// </summary>
+    /// <returns>新しいオーディオレンダラ</returns>
+    private AsioOut UpdateAudioRender()
+    {
+        this.ReleaseRender();
+
+        var newRender = this._audioRender = new AsioOut(this._driverName);
+
+        newRender.PlaybackStopped += this.RaisePlaybackStopped;
+
+        return newRender;
+    }
+
+    /// <summary>
+    /// 再生
+    /// </summary>
+    public override void Play()
+    {
+        Invoke(() => this._audioRender?.Play());
+    }
+
+    /// <summary>
+    /// 一時停止
+    /// </summary>
+    public override void Pause()
+    {
+        Invoke(() => this._audioRender?.Pause());
+    }
+
+    /// <summary>
+    /// 停止
+    /// </summary>
+    public override void Stop()
+    {
+        Invoke(() => this._audioRender?.Stop());
+    }
+
+    /// <summary>
+    /// 使用中のレンダラを解放する
+    /// </summary>
+    private void ReleaseRender()
+    {
+        var render = this._audioRender;
+        if (render is not null)
+        {
+            render.PlaybackStopped -= this.RaisePlaybackStopped;
+            render.Dispose();
+        }
+    }
+
+    /// <summary>
+    /// インスタンスを破棄する
+    /// </summary>
+    protected override void Dispose()
+    {
+        if (this._audioRender is not null)
+        {
+            this._audioRender?.Dispose();
+            this._audioRender = null;
+        }
+    }
+}
---
+++ Gouter/Devices/DirectSoundAudioDevice.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using NAudio.Wave;
+
+namespace Gouter.Devices;
+
+/// <summary>
+/// DirectSound音声出力デバイス
+/// </summary>
+internal class DirectSoundAudioDevice : AudioDevice
+{
+    /// <summary>
+    /// オーディオデバイス
+    /// </summary>
+    private Guid _deviceGuid;
+    private DirectSoundOut _audioRender;
+
+    /// <summary>
+    /// constructor
+    /// </summary>
+    /// <param name="deviceGuid"></param>
+    public DirectSoundAudioDevice(Guid deviceGuid)
+    {
+        this._deviceGuid = deviceGuid;
+    }
+
+    /// <summary>
+    /// 再生状態
+    /// </summary>
+    public override PlaybackState PlaybackState => this._audioRender?.PlaybackState ?? PlaybackState.Stopped;
+
+    /// <summary>
+    /// オーディオソースを設定する
+    /// </summary>
+    /// <param name="waveProvider"></param>
+    public override void SetAudioSource(ISampleProvider waveProvider) => Invoke(() =>
+    {
+        var render = this.UpdateAudioRender();
+
+        render.Init(waveProvider);
+    });
+
+    /// <summary>
+    /// オーディオレンダラを更新する
+    /// </summary>
+    /// <returns>新しいオーディオレンダラ</returns>
+    private DirectSoundOut UpdateAudioRender()
+    {
+        this.ReleaseRender();
+
+        var newRender = this._audioRender = new DirectSoundOut(this._deviceGuid);
+        newRender.PlaybackStopped += this.RaisePlaybackStopped;
+
+        return newRender;
+    }
+
+    /// <summary>
+    /// 再生
+    /// </summary>
+    public override void Play()
+    {
+        Invoke(() => this._audioRender?.Play());
+    }
+
+    /// <summary>
+    /// 一時停止
+    /// </summary>
+    public override void Pause()
+    {
+        Invoke(() => this._audioRender?.Pause());
+    }
+
+    /// <summary>
+    /// 停止
+    /// </summary>
+    public override void Stop()
+    {
+        Invoke(() => this._audioRender?.Stop());
+    }
+
+    /// <summary>
+    /// 使用中のレンダラを解放する
+    /// </summary>
+    private void ReleaseRender()
+    {
+        var render = this._audioRender;
+        if (render is not null)
+        {
+            render.PlaybackStopped -= this.RaisePlaybackStopped;
+            render.Dispose();
+        }
+    }
+
+    /// <summary>
+    /// インスタンスを破棄する
+    /// </summary>
+    protected override void Dispose()
+    {
+        if (this._audioRender is not null)
+        {
+            this._audioRender?.Dispose();
+            this._audioRender = null;
+        }
+    }
+}
